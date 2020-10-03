6c41a1d6e56f23969fc12fe7ae58454b88209dde
Anto <antonia.arriagada@usach.cl>
2020-10-03 04:26:48 -0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fist-app';
+  show:boolean = false;
+  showDos:boolean = true;
+  activo:string = '';
+
+  cursos:string[] = ['HTML', 'ANGULAR', 'PHP']
+  animales:Array<any> = [
+    {tipo:'Perro', nombre:'Lisa', edad:10},
+    {tipo:'Gato', nombre:'Pepon', edad:2},
+    {tipo:'Pato', nombre:'TanTan', edad:7}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { RouterModule, Routes} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FooterComponent } from './footer/footer.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
+const routes:Routes = [
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent,
+    CuerpoComponent,
+    ContactoComponent,
+    Cuerpo2Component,
+    InicioComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
