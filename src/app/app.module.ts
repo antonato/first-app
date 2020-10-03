a5d081d0b843e1da14ac81256aa0196f931e5d80
Anto <antonia.arriagada@usach.cl>
2020-10-03 17:44:17 -0300

    Fin del Proyecto Video #16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { RouterModule, Routes} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { EquipoService } from './equipo.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
@@ -11,9 +13,14 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
+import { EquipoComponent } from './equipo/equipo.component';
 
 const routes:Routes = [
-  {path: 'contacto', component: ContactoComponent}
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'equipo/:id', component: EquipoComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: '', component: InicioComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: '/', pathMatch: 'full'},
 ];
 
 @NgModule({
@@ -25,14 +32,17 @@ const routes:Routes = [
     ContactoComponent,
     Cuerpo2Component,
     InicioComponent,
-    NosotrosComponent
+    NosotrosComponent,
+    EquipoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    EquipoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
